def94d5e563951fdcee29565d88613e624a89b82
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-12-10 16:54:12 +0700

    update Database.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import com.company.DatabaseConnection.Database;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hihi");
+        new Database();
     }
 }
