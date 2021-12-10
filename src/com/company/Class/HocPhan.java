eb5fceb4df23ec612f780b483e10140f5e5b0110
ltnguyen1710 <74306963+ltnguyen1710@users.noreply.github.com>
2021-12-10 18:08:22 +0700

    update class hocphan, nhomlophoc

---
+++ src/com/company/Class/HocPhan.java
@@ -0,0 +1,61 @@
+package com.company.Class;
+
+public class HocPhan {
+    private String MMH;
+    private String tenmonhoc;
+    private String mabomon;
+    private int sotinchi;
+    private int sotiet;
+    private int sotietthuchanh;
+
+    public HocPhan() {
+    }
+
+    public String getMMH() {
+        return MMH;
+    }
+
+    public void setMMH(String MMH) {
+        this.MMH = MMH;
+    }
+
+    public String getTenmonhoc() {
+        return tenmonhoc;
+    }
+
+    public void setTenmonhoc(String tenmonhoc) {
+        this.tenmonhoc = tenmonhoc;
+    }
+
+    public String getMabomon() {
+        return mabomon;
+    }
+
+    public void setMabomon(String mabomon) {
+        this.mabomon = mabomon;
+    }
+
+    public int getSotinchi() {
+        return sotinchi;
+    }
+
+    public void setSotinchi(int sotinchi) {
+        this.sotinchi = sotinchi;
+    }
+
+    public int getSotiet() {
+        return sotiet;
+    }
+
+    public void setSotiet(int sotiet) {
+        this.sotiet = sotiet;
+    }
+
+    public int getSotietthuchanh() {
+        return sotietthuchanh;
+    }
+
+    public void setSotietthuchanh(int sotietthuchanh) {
+        this.sotietthuchanh = sotietthuchanh;
+    }
+}
---
+++ src/com/company/Class/NhomLopHoc.java
@@ -0,0 +1,61 @@
+package com.company.Class;
+
+public class NhomLopHoc {
+    private String Manhomlop;
+    private int Nhom;
+    private int thuchanh;
+    private String MMH;
+    private int SLdangki;
+    private int SLtkb;
+
+    public NhomLopHoc() {
+    }
+
+    public String getManhomlop() {
+        return Manhomlop;
+    }
+
+    public void setManhomlop(String manhomlop) {
+        Manhomlop = manhomlop;
+    }
+
+    public int getNhom() {
+        return Nhom;
+    }
+
+    public void setNhom(int nhom) {
+        Nhom = nhom;
+    }
+
+    public int getThuchanh() {
+        return thuchanh;
+    }
+
+    public void setThuchanh(int thuchanh) {
+        this.thuchanh = thuchanh;
+    }
+
+    public String getMMH() {
+        return MMH;
+    }
+
+    public void setMMH(String MMH) {
+        this.MMH = MMH;
+    }
+
+    public int getSLdangki() {
+        return SLdangki;
+    }
+
+    public void setSLdangki(int SLdangki) {
+        this.SLdangki = SLdangki;
+    }
+
+    public int getSLtkb() {
+        return SLtkb;
+    }
+
+    public void setSLtkb(int SLtkb) {
+        this.SLtkb = SLtkb;
+    }
+}
