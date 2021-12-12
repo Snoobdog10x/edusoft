a97c688237145a563013b770bdc294a6bed011cb
nghiaMeo <73874352+nghiaMeo@users.noreply.github.com>
2021-12-12 11:05:00 +0700

    create class PhongHoc uiux LapKHGD.java

--- src/com/company/UIUX/FunctionPanel/LapKHGD.java
+++ src/com/company/UIUX/FunctionPanel/LapKHGD.java
@@ -1,6 +1,26 @@
 package com.company.UIUX.FunctionPanel;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LapKHGD extends JPanel {
+    JTable KHGD;
+    JButton add,search;
+    JTextField searchAll;
+    JScrollPane sp = new JScrollPane();
+    public LapKHGD() {
+        InitUI();
+    }
+    private void InitUI(){
+        searchAll = new JTextField("nhập thông tin cần tìm");
+        search = new JButton("Tìm kiếm");
+        add(searchAll,BorderLayout.NORTH);
+        add(search, BorderLayout.NORTH);
+        add = new JButton("Them");
+        //sp.add(KHGD);
+        //add(KHGD,BorderLayout.CENTER);
+        KHGD = new JTable();
+        setVisible(true);
+
+    }
 }
