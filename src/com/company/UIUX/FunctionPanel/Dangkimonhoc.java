0cd5da73e13d400d63b261bb483aa35194f71955
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-12-12 21:17:58 +0700

    update UI DKMH

--- src/com/company/UIUX/FunctionPanel/Dangkimonhoc.java
+++ src/com/company/UIUX/FunctionPanel/Dangkimonhoc.java
@@ -1,58 +1,79 @@
 package com.company.UIUX.FunctionPanel;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Dangkimonhoc extends JPanel {
+public class Dangkimonhoc extends JPanel implements ActionListener {
     private BorderLayout Mainlayout = new BorderLayout();
-    private JPanel LeftPanel = new LeftPanel();
-    private JPanel CenterPanel = new CenterPanel();
+    private JPanel LeftPanel;
+    private JPanel CenterPanel;
+    public JPanel BottomPanel;
+    private JButton add = new JButton("Đăng ký môn học");
+    private JButton reload = new JButton("Tải lại bảng");
+    private JButton update = new JButton("Cập nhật đăng kí");
+    public JTable MainTable;
+    public JScrollPane MainScroll;
+    public BorderLayout MainLayout = new BorderLayout();
 
     public Dangkimonhoc() {
+        init();
+    }
+
+    //Start Init Panel
+    private void init() {
+        loadTable();
+        addEvent();
+        BottomPanel();
+        LeftPanel();
+        CenterPanel();
         setLayout(Mainlayout);
         add(LeftPanel, BorderLayout.WEST);
         add(CenterPanel, BorderLayout.CENTER);
     }
-    //CenterPanel
-    private class CenterPanel extends JPanel {
-        private JTable MainTable = new JTable();
-        private JScrollPane MainScroll = new JScrollPane(MainTable);
-        private BorderLayout MainLayout = new BorderLayout();
-        private BottomPanel BottomPanel = new BottomPanel();
 
-        public CenterPanel() {
-            setLayout(MainLayout);
-            add(MainScroll, BorderLayout.CENTER);
-            add(BottomPanel, BorderLayout.SOUTH);
-        }
-    }
-    //BottomPanel
-    private class BottomPanel extends JPanel implements ActionListener {
-        private JButton add = new JButton("Đăng ký môn học");
-        private JButton update = new JButton("Cập nhật đăng kí");
-        private JPanel ParentPanel;
-
-        public BottomPanel() {
-            add(add);
-            add(update);
-            add.addActionListener(this);
-            update.addActionListener(this);
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == add) {
-
-            }
-            if (e.getSource() == update) {
-
-            }
-
-        }
-    }
-    //LeftPanel
-    private class LeftPanel extends JPanel {
+    private void loadTable() {
+        MainTable = new JTable();
+        MainScroll = new JScrollPane(MainTable);
     }
 
+    private void BottomPanel() {
+        BottomPanel = new JPanel();
+        BottomPanel.add(add);
+        BottomPanel.add(update);
+    }
+
+    private void CenterPanel() {
+        CenterPanel = new JPanel();
+        CenterPanel.setLayout(MainLayout);
+        CenterPanel.add(MainScroll, BorderLayout.CENTER);
+        CenterPanel.add(BottomPanel, BorderLayout.SOUTH);
+    }
+
+    private void LeftPanel() {
+        LeftPanel = new JPanel();
+    }
+
+    private void addEvent() {
+        add.addActionListener(this);
+        update.addActionListener(this);
+        reload.addActionListener(this);
+    }
+
+    //End Init Panel
+    //Start Event
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == add) {
+            System.out.println("hiih");
+        }
+        if (e.getSource() == update) {
+            System.out.println("hiih");
+        }
+        if (e.getSource() == reload) {
+            System.out.println("hiih");
+        }
+    }
+    //End Event
 }
