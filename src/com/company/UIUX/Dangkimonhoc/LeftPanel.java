d82f125805b1643a3598bcbf421111f7f5d60223
Thanh Duy Nguyen <67373941+Snoobdog10x@users.noreply.github.com>
2021-12-12 19:27:59 +0700

    Create Đăng kí môn học Panel

---
+++ src/com/company/UIUX/Dangkimonhoc/LeftPanel.java
@@ -0,0 +1,4 @@
+package com.company.UIUX.Dangkimonhoc;
+
+public class LeftPanel {
+}
--- src/com/company/UIUX/FunctionPanel/Dangkimonhoc.java
+++ src/com/company/UIUX/FunctionPanel/Dangkimonhoc.java
@@ -1,11 +1,15 @@
 package com.company.UIUX.FunctionPanel;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Dangkimonhoc extends JPanel implements ActionListener {
-
+    private BorderLayout Mainlayout=new BorderLayout();
+    private JPanel  LeftPanel;
+    private JPanel  CenterPanel;
+    private JPanel  BottomPanel;
     @Override
     public void actionPerformed(ActionEvent e) {
         
