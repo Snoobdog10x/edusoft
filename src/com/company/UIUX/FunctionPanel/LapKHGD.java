da287eeb4a05f0071708f9c3dce6c7d471690a21
holakaka <74349746+holakaka@users.noreply.github.com>
2021-12-17 14:27:26 +0700

    update lkhgd

--- src/com/company/UIUX/FunctionPanel/LapKHGD.java
+++ src/com/company/UIUX/FunctionPanel/LapKHGD.java
@@ -6,16 +6,18 @@ import com.company.Process.ProcessLKHGD;
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import com.company.UIUX.KHGD.FrameAdd;
 import com.company.UIUX.KHGD.FrameUpdate;
 
-public class LapKHGD extends JPanel implements ActionListener{
+public class LapKHGD extends JPanel implements ActionListener, MouseListener {
     private BorderLayout Mainlayout = new BorderLayout();
     private JPanel CenterPanel;
     public JPanel BottomPanel;
@@ -24,10 +26,12 @@ public class LapKHGD extends JPanel implements ActionListener{
     private JTable MainTable;
     private JScrollPane MainScroll;
     private TableRowSorter<TableModel> rowSorter;
-    private ProcessLKHGD ProcessLKHGD= new ProcessLKHGD();
+    private ProcessLKHGD ProcessLKHGD = new ProcessLKHGD();
     private BorderLayout MainLayout = new BorderLayout();
 
-    public LapKHGD() { init();}
+    public LapKHGD() {
+        init();
+    }
 
     //Start Init Panel
     private void init() {
@@ -38,9 +42,10 @@ public class LapKHGD extends JPanel implements ActionListener{
         setLayout(Mainlayout);
         add(CenterPanel, BorderLayout.CENTER);
     }
+
     private void loadTable() {
-        MainTable = new JTable(ProcessLKHGD.loadTableModel()){
-            public boolean editCellAt (int row, int column, java.util.EventObject e) {
+        MainTable = new JTable(ProcessLKHGD.loadTableModel()) {
+            public boolean editCellAt(int row, int column, java.util.EventObject e) {
                 return false;
             }
         };
@@ -49,11 +54,12 @@ public class LapKHGD extends JPanel implements ActionListener{
     }
 
     private JTextField jtfFilter = new JTextField();
-    public void BottomPanel(){
-        Dimension size= Toolkit.getDefaultToolkit().getScreenSize();
-        int screenwidth= (int) (size.width);
-        int screenheight= (int) (size.height);
-        jtfFilter.setPreferredSize(new Dimension((int) (screenwidth*0.14), (int) (screenheight*0.02)));
+
+    public void BottomPanel() {
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenwidth = (int) (size.width);
+        int screenheight = (int) (size.height);
+        jtfFilter.setPreferredSize(new Dimension((int) (screenwidth * 0.14), (int) (screenheight * 0.02)));
         BottomPanel = new JPanel();
         BottomPanel.add(add);
         BottomPanel.add(reload);
@@ -88,6 +94,7 @@ public class LapKHGD extends JPanel implements ActionListener{
             }
         });
     }
+
     private void CenterPanel() {
         CenterPanel = new JPanel();
         CenterPanel.setLayout(MainLayout);
@@ -98,6 +105,7 @@ public class LapKHGD extends JPanel implements ActionListener{
     private void addEvent() {
         add.addActionListener(this);
         reload.addActionListener(this);
+        MainTable.addMouseListener(this);
     }
 
     //End Init Panel
@@ -111,24 +119,55 @@ public class LapKHGD extends JPanel implements ActionListener{
             reloadtable();
         }
     }
-    private void reloadtable(){
+
+    private void reloadtable() {
         MainTable.setModel(ProcessLKHGD.loadTableModel());
     }
+
     //End Event
-    private void addAction(){
+    private void addAction() {
 
     }
+
     private void clickTable() {
-        int row = MainTable.getSelectedRow();
         FrameUpdate up = new FrameUpdate();
-        String[] a={};
+        int row = MainTable.getSelectedRow();
+        String a[] = new String[11];
         if (row >= 0) {
-            for (int i = 0; i < 8; i++) {
-                a[i] = MainTable.getValueAt(row,i).toString();
+            for (int i = 0; i < 11; i++) {
+                a[i] = MainTable.getValueAt(row, i).toString();
+                System.out.println(MainTable.getValueAt(row, i).toString());
             }
-            up.setJComboBox(a);
+
         }
-        System.out.println(row);
+        up.uiux(a);
     }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        clickTable();
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+
 }
 
