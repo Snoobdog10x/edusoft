04db859da65057b913dafe49c206c2ca59dec8c0
holakaka <74349746+holakaka@users.noreply.github.com>
2021-12-23 14:56:20 +0700

    report vienchuc

--- src/com/company/Process/ProcessQLVC.java
+++ src/com/company/Process/ProcessQLVC.java
@@ -4,8 +4,15 @@ import com.company.Class.HocPhan;
 import com.company.Class.Vienchuc;
 import com.company.DatabaseConnection.MonHocDatabase;
 import com.company.DatabaseConnection.VienchucDatabse;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import javax.swing.table.DefaultTableModel;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.List;
 
 public class ProcessQLVC {
@@ -17,6 +24,52 @@ public class ProcessQLVC {
         VienchucDatabse vcdb=new VienchucDatabse();
         lvc=vcdb.getListVC();
         vcdb.closedb();
+    }
+    public void ExportPDF(){
+        Document document = new Document();
+        try
+        {
+            PdfWriter writer = PdfWriter.getInstance(document, new FileOutputStream("FILE BÁO CÁO VIÊN CHỨC.pdf"));
+            document.open();
+            document.add(new Paragraph("Danh Sách viên chức"));
+            PdfPTable table = new PdfPTable(8); // 3 columns.
+            table.setWidthPercentage(100); //Width 100%
+            table.setSpacingBefore(10f); //Space before table
+            table.setSpacingAfter(0f); //Space after table
+
+            //Set Column widths
+            float[] columnWidths = {1f, 1f, 1f,1f, 1f, 1f,1f, 1f};
+            table.setWidths(columnWidths);
+            for(int col=0;col<8 ; col++){
+                String[] colum = new String[]{"Mã Viên Chức","Loại Viên Chức", "Tên Viên Chức","Họ lót", "SĐT","Ngày Sinh", "Nơi Sinh", "Email"};
+                table.addCell(colum[col]);
+            }
+            loadListVienChuc();
+            //for(int aw=0;aw<16 ; aw++){
+            //    table.addCell("hi");
+            //}
+            for(Vienchuc i:lvc){
+                
+                    table.addCell(String.valueOf(i.getMVC()));
+                    table.addCell(i.getLoaivc());
+                    table.addCell(i.getTen());
+                    table.addCell(i.getHolot());
+                    table.addCell(i.getSdt());
+                    table.addCell(String.valueOf(i.getNgaysinh()));
+                    table.addCell(i.getNoisinh());
+                    table.addCell(i.getEmail());
+            }
+
+            document.add(table);
+            document.close();
+            writer.close();
+        } catch (DocumentException e)
+        {
+            e.printStackTrace();
+        } catch (FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
     }
     public DefaultTableModel loadTableModel(){
         loadListVienChuc();
